dff5d134f03c027029b4e30ed4384c79980bf09a
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-01 11:20:34 +0100

    first commit

---
+++ files/programs.cs
@@ -0,0 +1,17 @@
+using System;
+//learning c#
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)");  
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Spiro the Specter");
+     }
+  }
+}
